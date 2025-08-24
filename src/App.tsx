9dd8d824e99d0d251a5959448e4c43d53a3feff3
Ramprasad yadav <yadavramprasad563@gmail.com>
2025-08-24 15:49:08 +0530

    Navbar completed .

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./components/pages/Home"
+import Collection from "./components/pages/Collection"
+import About from "./components/pages/About"
+import Contact from "./components/pages/Contact"
+import Products from "./components/pages/Products"
+import Cart from "./components/pages/Cart"
+import Login from "./components/pages/Login"
+import PlaceOrder from "./components/pages/PlaceOrder"
+import Orders from "./components/pages/Orders"
+import Navbar from "./components/Navbar/Navbar"
 
 function App() {
 
   return (
     <>
-     <div>
-      Hello
+     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] " >
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/collection" element={<Collection/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/product/:productId" element={<Products/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/place-order" element={<PlaceOrder/>} />
+          <Route path="/orders" element={<Orders/>} />
+
+        </Routes>
      </div>
     </>
   )
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,66 @@
+import { ChevronDown, Menu, Search, ShoppingCart, User } from "lucide-react"
+import Logo from "../../assets/log.png"
+import { Link, NavLink } from 'react-router-dom'
+import { useState } from "react"
+
+const Navbar = () => {
+    const [visible, setvisible] = useState(false)
+  return (
+    <div className='flex items-center justify-between py-5 font-medium' >
+       <img src={Logo} alt='Wear' className='w-36' />
+       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+        <NavLink to="/" className="flex flex-col items-center gap-1" >
+        <p>Home</p>
+        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden ' />
+        </NavLink>
+        <NavLink to="/collection" className="flex flex-col items-center gap-1" >
+        <p>Colletion</p>
+        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700  hidden' />
+        </NavLink>
+        <NavLink to="/about" className="flex flex-col items-center gap-1" >
+        <p>About</p>
+        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700  hidden' />
+        </NavLink>
+        <NavLink to="/contact" className="flex flex-col items-center gap-1" >
+        <p>Contact</p>
+        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden ' />
+        </NavLink>
+       </ul>
+       <div className="flex items-center gap-6">
+       <Search  className="w-5 cursor-pointer"/>
+       <div className="group relative">
+         <User className="w-5 cursor-pointer" />
+         <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4 " >
+         <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded  ">
+          <p className="cursor-pointer hover:text-black" >My Profile</p>
+          <p className="cursor-pointer hover:text-black" >Orders</p>
+          <p className="cursor-pointer hover:text-black" >Logout</p>
+         </div>
+         </div>
+       </div>
+       <Link to="/cart" className="relative" >
+         <ShoppingCart className="w-5 min-w-5" />
+         <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]  " >10</p>
+       </Link>
+       <Menu onClick={()=>setvisible(true)} className=" w-5 cursor-pointer sm:hidden" />
+       </div>
+       {/* Mobile Navabr  */}
+       <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0' }`} >
+              <div className="flex flex-col text-gray-700" >
+                <div onClick={()=>setvisible(false)} className="flex items-center gap-2 p-3" >
+                  <ChevronDown className="h-4 rotate-90 mt-[3px]"/>
+                  <p>Back</p>
+                </div>
+                <NavLink onClick={()=>setvisible(false)}  className="py-2 pl-6 border-b" to="/home" >Home</NavLink>
+                <NavLink  onClick={()=>setvisible(false)} className="py-2 pl-6 border-b" to="/collection" >Collection</NavLink>
+                <NavLink onClick={()=>setvisible(false)}  className="py-2 pl-6 border-b" to="/about" >About</NavLink>
+                <NavLink onClick={()=>setvisible(false)}  className="py-2 pl-6 " to="/contact" >Contact</NavLink>
+                {/* <NavLink className="py-2 pl-6 border" to="/home" > Home</NavLink> */}
+              </div>
+       </div>
+    </div>
+
+  )
+}
+
+export default Navbar
