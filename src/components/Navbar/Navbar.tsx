f43f871495ed3ff8a70eaa0dbd6c8ad23274fe38
Ramprasadya <yadavramprasad563@gmail.com>
2025-10-06 20:19:06 +0530

    navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -59,7 +59,7 @@ const Navbar = () => {
               <div className="group-hover:block hidden absolute dropdown-menu -right-[50px] pt-2 " >
                 <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded  ">
                   <p className="cursor-pointer hover:text-black" >My Profile</p>
-                  <p onClick={()=>navigate("/orders")} className="cursor-pointer hover:text-black" >Orders</p>
+                  <p onClick={() => navigate("/orders")} className="cursor-pointer hover:text-black" >Orders</p>
                   <p onClick={Logout} className="cursor-pointer hover:text-black" >Logout</p>
                 </div>
               </div>
@@ -83,6 +83,10 @@ const Navbar = () => {
           <NavLink onClick={() => setvisible(false)} className="py-2 pl-6 border-b" to="/collection" >Collection</NavLink>
           <NavLink onClick={() => setvisible(false)} className="py-2 pl-6 border-b" to="/about" >About</NavLink>
           <NavLink onClick={() => setvisible(false)} className="py-2 pl-6 " to="/contact" >Contact</NavLink>
+           <NavLink onClick={() => setvisible(false)}  className="py-2 pl-6 " to="/orders" >Orders</NavLink>
+         <div className="w-full flex justify-center relative" >
+          <button onClick={Logout} className=" absolute top-[150px] cursor-pointer py-2 px-4 bg-black text-white w-[80%] " >Logout</button>
+          </div> 
           {/* <NavLink className="py-2 pl-6 border" to="/home" > Home</NavLink> */}
         </div>
       </div>
