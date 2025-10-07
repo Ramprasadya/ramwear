f51575410b4419832b83b5c3368820df1bae3062
Ramprasad yadav <yadavramprasad563@gmail.com>
2025-10-07 11:16:15 +0530

    icon update

--- src/components/UiComponents/OurPolicity.tsx
+++ src/components/UiComponents/OurPolicity.tsx
@@ -1,20 +1,22 @@
-import { BadgeCent, Headset, ShieldCheck } from 'lucide-react'
+import { BadgeCentIcon } from '../ui/BadgeCentIcon'
+import { HeadsetIcon } from '../ui/HeadsetIcon'
+import { ShieldCheckIcon } from '../ui/ShieldCheckIcon'
 
 const OurPolicity = () => {
   return (
     <div className=' flex flex-col sm:flex-row justify-around gap-12 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700 ' >
        <div>
-          <BadgeCent className='w-12 m-auto mb-5' />
+          <BadgeCentIcon  className='w-12 m-auto mb-5' />
           <p className='font-semibold' >Easy Exchange Policy</p>
           <p className='text-gray-400' >We offer hassle free exchange policy</p>
        </div>
        <div>
-          <ShieldCheck className='w-12 m-auto mb-5' />
+          <ShieldCheckIcon className='w-12 m-auto mb-5' />
           <p className='font-semibold' >7 Days Return Policy</p>
           <p className='text-gray-400' >We Provide 7 Days free return policy</p>
        </div>
        <div>
-          <Headset className='w-12 m-auto mb-5' />
+          <HeadsetIcon className='w-12 m-auto mb-5' />
           <p className='font-semibold' >Best customer support</p>
           <p className='text-gray-400' >We provide 24/7  customer support</p>
        </div>
